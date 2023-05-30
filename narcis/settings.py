60b109719b6544d7b7940b1ef2d7cc7f47c2d3e5
Anivaries <k0m4r4c00@gmail.com>
2023-05-30 18:02:30 +0200

    html fix maybe

--- narcis/settings.py
+++ narcis/settings.py
@@ -127,12 +127,10 @@ USE_I18N = True
 
 USE_TZ = True
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static/'),
 #     # BASE_DIR / "static",
